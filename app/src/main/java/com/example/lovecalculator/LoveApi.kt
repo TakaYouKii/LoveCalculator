d7109c5b0f5676c1d40b35083eff70dda26cac7b
TakaYouKii <avwro1708@gmaol.cpm>
2023-08-23 21:32:40 +0600

    Lesson 5.2 commit 1

---
+++ app/src/main/java/com/example/lovecalculator/LoveApi.kt
@@ -0,0 +1,18 @@
+package com.example.lovecalculator
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+
+interface LoveApi {
+
+    @GET("getPercentage")
+    fun calculateMatching(
+        @Query("fname") firstName:String,
+        @Query("sname") secondName:String,
+        @Header("X-RapidAPI-Key") key: String = "ce39dcf001msh31f1e21f23fb445p1d755fjsn19e38417b35b",
+        @Header("X-RapidAPI-Host") host: String = "love-calculator.p.rapidapi.com",
+    ): Call<LoveModel>
+}
---
+++ app/src/main/java/com/example/lovecalculator/ResultFragment.kt
@@ -0,0 +1,35 @@
+package com.example.lovecalculator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lovecalculator.databinding.FragmentResultBinding
+
+@Suppress("DEPRECATION")
+class ResultFragment : Fragment() {
+
+
+    lateinit var binding: FragmentResultBinding
+    private lateinit var loveModel: LoveModel
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentResultBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loveModel = this.arguments?.getSerializable("love") as LoveModel
+        binding.tvFirstName.text = loveModel.firstName
+        binding.tvSecondName.text = loveModel.secondName
+        binding.tvPercentage.text = loveModel.percentage + "%"
+        binding.tvResult.text = loveModel.result
+
+    }
+
+}
